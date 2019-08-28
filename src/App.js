75bbcf067110eb53184b31bcabbbf6f618883e50
Jonathan Bogdanowicz <kamil.czoska1997@outlook.com>
2019-08-28 01:28:55 -0300

    Work on jumbotron

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import Header from './components/layout/Header'
+import HomeSection from './components/HomeSection';
+
+import './App.css';
 
 class App extends React.Component {
   render() {
     return (
       <>
         <Header />
+        <HomeSection />
       </>
     )
   }
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -10,10 +10,10 @@ class Header extends React.Component {
         <Navbar.Brand href="#home">Portfolio</Navbar.Brand>
         <Nav className="justify-content-center header">
           <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#features">Skills</Nav.Link>
-          <Nav.Link href="#pricing">Projects</Nav.Link>
-          <Nav.Link href="#pricing">Testimonials</Nav.Link>
-          <Nav.Link href="#pricing">About</Nav.Link>
+          <Nav.Link href="#skills">Skills</Nav.Link>
+          <Nav.Link href="#projects">Projects</Nav.Link>
+          <Nav.Link href="#testimonials">Testimonials</Nav.Link>
+          <Nav.Link href="#about">About</Nav.Link>
         </Nav>
       </Navbar>
     );
